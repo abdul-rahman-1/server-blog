8aa27c75812009c4a6d37d1a7263e845f1b46b9b
Abdul Rahman <105973330+abdul-rahman-1@users.noreply.github.com>
2025-04-23 19:15:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from "dotenv"; 
 dotenv.config({ path: './.env' });
 
 const app = express();
